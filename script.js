cee0de5d5d353a260419703776a24fc5b324ccdc
Falze Labib <fazle.labib@gmail.com>
2023-12-08 19:35:55 +0600

    Fix indentation of showTasks

--- script.js
+++ script.js
@@ -23,78 +23,81 @@ function saveTasks(tasks) {
 
 function showTasks() {
 
-    const remainingTasks = document.querySelector('.remaining-tasks');
+  const remainingTasks = document.querySelector('.remaining-tasks');
 
-    remainingTasks.innerHTML = '';
+  remainingTasks.innerHTML = '';
   
-    const tasks = getTasks();
+  const tasks = getTasks();
 
-    let taskCount = tasks.length;
-    itemsLeft.innerHTML = `${taskCount} items left`;
+  let taskCount = tasks.length;
+  itemsLeft.innerHTML = `${taskCount} items left`;
   
-    tasks.forEach((task, index) => {
-        const taskDiv = document.createElement('div');
-        taskDiv.classList.add('task');
+  tasks.forEach((task, index) => {
+
+    const taskDiv = document.createElement('div');
+    taskDiv.classList.add('task');
   
-        const checkboxContainer = document.createElement('div');
-        checkboxContainer.classList.add('checkbox-container');
+    const checkboxContainer = document.createElement('div');
+    checkboxContainer.classList.add('checkbox-container');
   
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        checkbox.setAttribute('id', `checkbox-${index + 1}`);
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', `checkbox-${index + 1}`);
       
   
-        const checkboxLabel = document.createElement('label');
-        checkboxLabel.setAttribute('for', `checkbox-${index + 1}`);
+    const checkboxLabel = document.createElement('label');
+    checkboxLabel.setAttribute('for', `checkbox-${index + 1}`);
   
-        checkboxContainer.appendChild(checkbox);
-        checkboxContainer.appendChild(checkboxLabel);
+    checkboxContainer.appendChild(checkbox);
+    checkboxContainer.appendChild(checkboxLabel);
   
-        const taskContentDiv = document.createElement('div');
-        taskContentDiv.classList.add('task-content');
+    const taskContentDiv = document.createElement('div');
+    taskContentDiv.classList.add('task-content');
   
-        const taskParagraph = document.createElement('p');
-        taskParagraph.textContent = task;
+    const taskParagraph = document.createElement('p');
+    taskParagraph.textContent = task;
   
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-task');
-        const deleteIcon = document.createElement('img');
-        deleteIcon.setAttribute('src', './images/icon-cross.svg');
-        deleteIcon.setAttribute('alt', 'Cross/Close icon');
-        deleteButton.appendChild(deleteIcon);
-      
-        deleteButton.addEventListener('click', () => {
-            deleteTask(index);
-        });
-  
-        taskContentDiv.appendChild(taskParagraph);
-        taskContentDiv.appendChild(deleteButton);
-  
-        taskDiv.appendChild(checkboxContainer);
-        taskDiv.appendChild(taskContentDiv);
-        
-        remainingTasks.appendChild(taskDiv);
-
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-task');
+    const deleteIcon = document.createElement('img');
+    deleteIcon.setAttribute('src', './images/icon-cross.svg');
+    deleteIcon.setAttribute('alt', 'Cross/Close icon');
+    deleteButton.appendChild(deleteIcon);
+    
+    deleteButton.addEventListener('click', () => {
+      deleteTask(index);
     });
+  
+    taskContentDiv.appendChild(taskParagraph);
+    taskContentDiv.appendChild(deleteButton);
+  
+    taskDiv.appendChild(checkboxContainer);
+    taskDiv.appendChild(taskContentDiv);
+        
+    remainingTasks.appendChild(taskDiv);
 
-    if (tasks.length === 0) {
-        noTasks.style.display = 'flex';
-    } else {
-        noTasks.style.display = 'none';
-    }
+    
 
-    // Strikes through completed tasks
-    const checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
-    for (const checkbox of checkboxes) {
-        checkbox.addEventListener('change', (event) => {
-          const taskContent = event.target.closest('.task').querySelector('.task-content p');
-          if (checkbox.checked) {
-            taskContent.style.textDecoration = 'line-through';
-          } else {
-            taskContent.style.textDecoration = 'none';
-          }
-        });
-    }
+  });
+
+  if (tasks.length === 0) {
+    noTasks.style.display = 'flex';
+  } else {
+    noTasks.style.display = 'none';
+  }
+
+  // Strikes through completed tasks
+  const checkboxes = document.querySelectorAll('.checkbox-container input[type="checkbox"]');
+  for (const checkbox of checkboxes) {
+    checkbox.addEventListener('change', (event) => {
+      const taskContent = event.target.closest('.task').querySelector('.task-content p');
+      if (checkbox.checked) {
+        taskContent.style.textDecoration = 'line-through';
+      } else {
+        taskContent.style.textDecoration = 'none';
+      }
+    });
+  }
     
 }
   
